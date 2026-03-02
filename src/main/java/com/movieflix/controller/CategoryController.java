0d5bd78dc14b8a73944b1c40aca23730031fe7a1
Yan Kauê <yankaue4@gmail.com>
2026-03-01 23:32:35 -0300

    Crud de catálogo de filmes, no Curso de Spring no Java10x.

---
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -0,0 +1,41 @@
+package com.movieflix.controller;
+
+import com.movieflix.entity.Category;
+import com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    public final CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.findAll();  //// chama o método findAll do service para buscar todas as categorias no banco
+    }
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryId(@PathVariable Long id) {
+        Optional<Category> optionalCategory = categoryService.findById(id);
+        if (optionalCategory.isPresent()) {
+            return optionalCategory.get();
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleterCategoryID(@PathVariable Long id){
+        categoryService.deleteCategory(id);
+    }
+}
