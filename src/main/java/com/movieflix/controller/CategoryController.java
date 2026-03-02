84b2809b15eb6f6c0a1217d7da36f610c11f025f
Yan Kauê <yankaue4@gmail.com>
2026-03-01 23:36:42 -0300

    Remoção de Comentário

--- src/main/java/com/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -17,7 +17,7 @@ public class CategoryController {
 
     @GetMapping
     public List<Category> getAllCategories() {
-        return categoryService.findAll();  //// chama o método findAll do service para buscar todas as categorias no banco
+        return categoryService.findAll(); 
     }
     @PostMapping
     public Category saveCategory(@RequestBody Category category) {
